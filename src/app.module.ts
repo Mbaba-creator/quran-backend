c8b024f77e34b49ae7817658f76c704470c2459c
Mbaba-creator <mostafa.abdi.algaddah@gmail.com>
2026-07-09 10:42:40 +0400

    Temp: Disable TypeORM to get Socket.IO working without DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,12 @@
 ﻿import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeormConfig } from './database/typeorm.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { SalasModule } from './modules/salas/salas.module';
 import { BibliotecaModule } from './modules/biblioteca/biblioteca.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      ...typeormConfig,
-      retryAttempts: 0,
-      synchronize: false,
-    }),
+    // TypeOrmModule temporarily disabled - Socket.IO doesn't need it
+    // TODO: Fix PostgreSQL connection issue and re-enable
     AuthModule,
     SalasModule,
     BibliotecaModule,
